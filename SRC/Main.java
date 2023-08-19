05ee92a96b6a21860cc3411e0798e10f64ee7132
KumaCod3 <kumacod3@proton.me>
2023-08-19 15:26:06 +0200

    create database

--- SRC/Main.java
+++ SRC/Main.java
@@ -1,5 +1,7 @@
 package SRC;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -10,7 +12,7 @@ public class Main {
 	private static Connection connection;
 	private static Statement statement;
 	private static String PASSWORD="Du1k3rKnows!";
-	private static String DataBaseNAME="negozioDB";
+	private static String DataBaseNAME="pro"/*+"negozioDB"*/;
 	
 	public static void main(String[] args) {
 		try {
@@ -18,13 +20,14 @@ public class Main {
 			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+DataBaseNAME+"?characterEncoding=latin1&useConfigs=maxPerformance","root",PASSWORD);
 			statement = connection.createStatement();
 		}catch (ClassNotFoundException e) {System.out.println("MySQL Library missing");}
-		catch (SQLException e) {System.out.println("MySQL DATABASE missing");}
-		System.out.println("CONNECTION ");
-		leggiCliID(0);
+		catch (SQLException e) {
+			System.out.println("MySQL DATABASE missing");
+			creaDB();
+			importaDB();
+		}
+		tryQuery(0);
 	}
-	
-	
-	static public String leggiCliID(int x) {
+	static public String tryQuery(int x) {
 		try {
 			String sql="SELECT * FROM Clienti WHERE ID_CLIENTE="+x;
 			ResultSet result = statement.executeQuery(sql);
@@ -33,20 +36,33 @@ public class Main {
 			while (result.next()) {
 				int id = result.getInt("ID_Cliente");
 				String nome=result.getString("name");
-				String cognome=result.getString("last_name");
-				String telefono=result.getString("phone");
-				String email=result.getString("Mail");
-				String iva=result.getString("vatn");
-				String stato=result.getString("state");
-				String citta=result.getString("city");
-				String indirizzo=result.getString("street");
-				Double saldo=result.getDouble("tot_sold");
-				String note=result.getString("note");
-				System.out.println(id+ nome+ cognome+ telefono+ email+ iva+ indirizzo+ saldo);
-				inser=id+","+nome+","+cognome+","+telefono+","+email+","+stato+","+citta+","+indirizzo+","+iva+","+saldo+","+note;
+				System.out.println(id+ nome);
+				inser=id+","+nome;
 			}
 			return inser;
 		}catch (SQLException e) {e.printStackTrace();}
 		return "Fail";
 	}
+	
+	static void creaDB() {
+		System.out.println("creoDB ");
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/?characterEncoding=latin1&useConfigs=maxPerformance", "root", PASSWORD);
+			Statement st = connection.createStatement();
+			String sql="CREATE DATABASE "+DataBaseNAME;
+			st.executeUpdate(sql);
+		}catch (SQLException e) { System.out.println("imposs creare DB");}
+	}
+	
+	private static void importaDB() {  
+		try {
+				connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+DataBaseNAME+"?characterEncoding=latin1&useConfigs=maxPerformance","root",PASSWORD);
+				statement = connection.createStatement();
+				
+				ScriptRunner runner = new ScriptRunner(connection , false, false);
+				runner.runScript(new BufferedReader(new FileReader("proDB.sql")));
+		} catch (Exception ex) {
+	        ex.printStackTrace();
+	    }
+	}   
 }
